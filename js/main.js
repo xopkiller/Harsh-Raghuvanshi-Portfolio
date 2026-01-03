61b1d1ed0d2d40dd7069017d4b8c2d0adeaa4d18
Xopkiller <raghuvanshiharsh515@gmail.com>
2026-01-04 00:31:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,5 +11,15 @@ gsap.utils.toArray("section").forEach((section) => {
         y: 60,
         duration: 1,
         ease: "power3.out"
-    });
+        document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("mousemove", e => {
+    const rect = btn.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    btn.style.transform = `translate(${x * 0.15}px, ${y * 0.15}px)`;
+  });
+
+  btn.addEventListener("mouseleave", () => {
+    btn.style.transform = "translate(0,0)";
+  });
 });
